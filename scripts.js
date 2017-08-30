0ab6d0358a27fb815edc859b5a00512e216d65d4
Andrew Siegman <blerrnk@gmail.com>
2017-08-30 12:38:46 -0500

    slight refactor, add copyright

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 
-function handleFiles(files, callback) {
+function handleFiles(files) {
     var imgs = [];
     for (i = 0; i < files.length; i++) {
         let file = files[i];
@@ -10,12 +10,39 @@ function handleFiles(files, callback) {
             imgs.push(this);
             window.URL.revokeObjectURL(this.src);
             if (imgs.length == files.length) {
-                callback(imgs);
+                fillTable(imgs);
                 return;
             }
         }
     }
 }
+
+function fillTable(imgs) {
+    $.each(imgs, function(i, img) {
+        let dpi = 300;
+
+        let fn  = img.origFileName,
+            mwi = (img.width / dpi).toFixed(2),
+            mhi = (img.height / dpi).toFixed(2),
+            mwc = (mwi * 2.54).toFixed(1),
+            mhc = (mhi * 2.54).toFixed(1);
+
+        let row = $("#template_row").clone().removeAttr("id");
+        row.find(".image").html(img);
+        row.find(".file").text(fn);
+        row.find(".display").text(img.width + " x " + img.height);
+        row.find(".print").text(mwi + " x " + mhi);
+
+        let more = $("#template_more").clone().removeAttr("id");
+
+        row.appendTo("#results #rows");
+        more.appendTo("#results #rows");
+
+    });
+
+    $("#results").show();
+}
+
 
 $(function() {
 
@@ -25,7 +52,6 @@ $(function() {
             .on('dragover', function(e) {
                 e.preventDefault();
                 $(this).addClass("dragging");
-                // $(this).removeClass("loading");
             })
             .on('dragend dragleave', function(e) {
                 e.preventDefault();
@@ -35,32 +61,7 @@ $(function() {
                 e.preventDefault();
                 let files = e.originalEvent.dataTransfer.files;
                 $(this).removeClass("dragging");
-                // $(this).addClass("loading");
-                let dpi = 300;
-                handleFiles(files, function(imgs) {
-                    // $("#filedrag").removeClass("loading");
-                    $.each(imgs, function(i, img) {
-                        let fn  = img.origFileName,
-                            mwi = (img.width / dpi).toFixed(2),
-                            mhi = (img.height / dpi).toFixed(2),
-                            mwc = (mwi * 2.54).toFixed(1),
-                            mhc = (mhi * 2.54).toFixed(1);
-
-                        let row = $("#template_row").clone().removeAttr("id");
-                        row.find(".image").html(img);
-                        row.find(".file").text(fn);
-                        row.find(".display").text(img.width + " x " + img.height);
-                        row.find(".print").text(mwi + " x " + mhi);
-
-                        let more = $("#template_more").clone().removeAttr("id");
-
-                        row.appendTo("#results #rows");
-                        more.appendTo("#results #rows");
-
-                    });
-
-                    $("#results").show();
-                });
+                handleFiles(files);
             });
     }
 
